c71c893b1a8e2437965b260354200c665af0c597
i22urvip <152216164+i22urvip@users.noreply.github.com>
2026-02-23 10:24:44 +0000

    Configuración a producción: PostgreSQL en Neon.tech y variables de entorno seguras

--- orquesta/settings.py
+++ orquesta/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+from dotenv import load_dotenv
+import dj_database_url
+
+# Esto le dice a Django que busque el archivo .env y lo lea
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +81,7 @@ WSGI_APPLICATION = "orquesta.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
